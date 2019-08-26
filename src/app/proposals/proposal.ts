504a358b151aa28ac01defa8f6282f48b313fc12
Oscar M. Olivera <oscarmolivera@gmail.com>
2019-08-26 17:43:03 -0400

    Crear Clase Proposal para interactuar con BackEnd

---
+++ src/app/proposals/proposal.ts
@@ -0,0 +1,14 @@
+export class Proposal{
+  constructor(
+    public id?: number,
+    public customer?: string,
+    public portfolio_url: string = 'http://',
+    public tools?: string,
+    public estimated_hours?: number,
+    public houly_rates?: number,
+    public weeks_to_complete?: number,
+    public client_email?: string
+  ){}
+}
+//sample data
+// 19, 'AseFarma SPA', 'http://portfolio.oscarmolivera.me', 'Angular4', 40, 6000clp, 1, 'jules.barros@asefarma.cl'
