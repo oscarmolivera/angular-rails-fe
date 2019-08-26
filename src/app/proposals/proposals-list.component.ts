1ac2895e9184a4e66581b23db278aabaf32955f3
Oscar M. Olivera <oscarmolivera@gmail.com>
2019-08-26 18:13:22 -0400

    implementato clase Proposal con data sample

--- src/app/proposals/proposals-list.component.ts
+++ src/app/proposals/proposals-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Proposal } from './proposal';
 
 @Component({
   selector: 'proposals-list',
@@ -10,5 +11,14 @@ export class ProposalsListComponent implements OnInit {
 
   ngOnInit() {
   }
+  proposalOne: Proposal = new Proposal(19, 'AseFarma SPA', 'http://portfolio.oscarmolivera.me', 'Angular4', 40, 6000, 1, 'jules.barros@asefarma.cl');
+  proposalTwo: Proposal = new Proposal(23, 'Miranda CL', 'http://portfolio.oscarmolivera.me', 'Angular4', 40, 6000, 1, 'sendos@miranda.cl');
+  proposalThree: Proposal = new Proposal(36, 'O\'Higgings', 'http://portfolio.oscarmolivera.me', 'Angular4', 40, 6000, 1, 'o.hilgh@ohiggings.com');
 
+  proposals: Proposal[] = [
+    this.proposalOne,
+    this.proposalTwo,
+    this.proposalThree
+  ]
+  
 }
