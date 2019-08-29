7a04b1f12d97b58efcdb4af390d3fd8cea46b735
Oscar M. Olivera <oscarmolivera@gmail.com>
2019-08-29 10:47:47 -0400

    Agregando Links con Activacion dinamica con routerLinkActive

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,7 +2,8 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app',
-  templateUrl: 'app.component.html'
+  templateUrl: 'app.component.html',
+  styles: [`.router-link-active { background-color: lightgray; }`]
 })
 
 export class AppComponent {
